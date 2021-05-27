7e2a12e25638aa88377ceb878cf6271a041b795b
Manuel <manuel.araujo1511@gmail.com>
2021-05-27 16:33:22 -0500

    add google charts & read file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,24 @@
 import { Component } from '@angular/core';
+import { GoogleChartInterface } from 'ng2-google-charts';
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
 
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +26,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TestLocationWorld';
+  fileContent: string = "";
+
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = ELEMENT_DATA;
+
+  public onChange(fileList: FileList): void {
+    let file = fileList[0];
+    let fileString:any= "";
+    let fileReader = new FileReader();
+
+    fileReader.onloadend = function(x) {
+      console.log(fileReader.result)
+
+      var lines = fileReader.result.split('\n');
+      
+      for(var line = 0; line < lines.length; line++){
+          console.log(lines[line]);
+      }
+      //self.fileContent = fileReader.result;
+    }
+    fileReader.readAsText(file);
+  }
+
+  public pieChart: GoogleChartInterface = {
+    chartType: 'ColumnChart',
+    dataTable: [
+      ['Country', 'Performance', 'Profits'],
+      ['Germany', 700, 1200],
+      ['USA', 300, 600],
+      ['Brazil', 400, 500],
+      ['Canada', 500, 1000],
+      ['France', 600, 1100],
+      ['RU', 800, 1000]
+    ],
+    options: {title: 'Countries'}
+  };
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,72 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { Ng2GoogleChartsModule } from 'ng2-google-charts';
+
+import { LayoutModule } from '@angular/cdk/layout';
+import {
+  MatAutocompleteModule,
+  MatCheckboxModule,
+  MatDatepickerModule,
+  MatRadioModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatNativeDateModule,
+  MatSelectModule,
+  MatToolbarModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatListModule,
+  MatGridListModule,
+  MatCardModule,
+  MatMenuModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatSortModule,
+  MatChipsModule,
+  MatFormFieldModule,
+  MatInputModule
+} from '@angular/material';
+
+const modules : any [] =[
+  LayoutModule,
+  MatAutocompleteModule,
+  MatCheckboxModule,
+  MatDatepickerModule,
+  MatRadioModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatNativeDateModule,
+  MatSelectModule,
+  MatToolbarModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatListModule,
+  MatGridListModule,
+  MatCardModule,
+  MatMenuModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatSortModule,
+  MatChipsModule,
+  MatFormFieldModule,
+  MatInputModule
+  
+]
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    Ng2GoogleChartsModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    modules
   ],
   providers: [],
   bootstrap: [AppComponent]
